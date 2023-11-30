721339a3d8916cb41cdbc0956a160bf9ec741aed
tejasmaramwar <tejasmaramwar01@gmail.com>
2023-11-30 22:50:20 +0530

    Added Infinite logo carousel

--- src/components/OurCustomer.js
+++ src/components/OurCustomer.js
@@ -1,14 +1,30 @@
-import 'owl.carousel/dist/assets/owl.carousel.css';
-import 'owl.carousel/dist/assets/owl.theme.default.css';
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
 import React, { Component } from "react";
-import OwlCarousel from 'react-owl-carousel';
-import './css/OurCustomer.css';
+import OwlCarousel from "react-owl-carousel";
+import "./css/OurCustomer.css";
 export default class OurCustomer extends Component {
   render() {
     return (
-      <div className='padding_to_section'>
-        <h1 className="text-center">Our Customer</h1>
-        <OwlCarousel className='owl-theme padding_to_header ' loop margin={10} nav>
+      <div className="padding_to_section">
+        <h1 className="text-center">Our Clients</h1>
+        <div className="logos">
+          <div className="logos-slide">
+            <img src="/images/clients/alkyl.png" alt=""/>
+            <img src="/images/clients/biopharmax.png" alt=""/>
+            <img src="/images/clients/dedietrich.png" alt=""/>
+            <img src="/images/clients/deepaknitrite.png" alt=""/>
+            <img src="/images/clients/gennova.png" alt=""/>
+            <img src="/images/clients/hikal.jpeg" alt=""/>
+            <img src="/images/clients/iioroorkee.jpeg" alt=""/>
+            <img src="/images/clients/ipca.png" alt=""/>
+            <img src="/images/clients/laxmiorganic.jpeg" alt=""/>
+            <img src="/images/clients/prasol.jpg" alt=""/>
+            <img src="/images/clients/rallis.png" alt=""/>
+            <img src="/images/clients/reliance.png" alt=""/>
+          </div>
+        </div>
+        {/* <OwlCarousel className='owl-theme padding_to_header ' loop margin={10} nav>
     <div class='item'>
         <h4>1</h4>
           </div>
@@ -21,8 +37,8 @@ export default class OurCustomer extends Component {
           <div class='item'>
         <h4>4</h4>
           </div>
-          </OwlCarousel>
+          </OwlCarousel> */}
       </div>
-    )
+    );
   }
 }

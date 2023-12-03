14b8c833f4fd378fd918f84bab93663825cf49ba
tejasmaramwar <tejasmaramwar01@gmail.com>
2023-12-03 21:23:01 +0530

    footer changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
-import "./App.css";
 import NavbarComp from "./components/NavbarComp";
+import AppFooter from "./components/Footer";
 
 function App() {
   return (
     <>
       <div className="App">
         <NavbarComp />
-        {/* <footer id="footer">
-        <AppFooter />
-      </footer> */}
+        <footer>
+          <AppFooter />
+        </footer>
       </div>
     </>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,27 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
+import "./css/footer.css";
 
 export default function AppFooter() {
   return (
     <>
-      <Container fluid>
-          <div className="copyright">Copyright | All rights reserved.</div>
-        </Container>
+      <div className="footerMain">
+        <div className="footerLeft">
+          <div>
+            <h3>
+              <span>
+                <img src="/images/weblogo.png" alt="" />
+              </span>
+              SEPREX PVT LTD.
+            </h3>
+          </div>
+          <p>
+            Seprex is one-stop Enginering Solution Company setup at Pune India
+            by passionate team of engineers.
+          </p>
+        </div>
+        <div className="footerRight"></div>
+      </div>
     </>
-  )
+  );
 }

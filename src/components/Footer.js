938345c7aafc34538f0ec3723452b8e5b6472b4b
tejasmaramwar <tejasmaramwar01@gmail.com>
2023-12-03 22:47:57 +0530

    footer final changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,26 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import "./css/footer.css";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import Button from "react-bootstrap/Button";
 
 export default function AppFooter() {
+  const handleFormSubmit = () => {
+    const name = document.getElementById("nameInput").value;
+    const email = document.getElementById("emailInput").value;
+    const message = document.getElementById("messageInput").value;
+
+    const subject = "Contact Form Submission";
+    const body = `Name: ${name}%0D%0AEmail: ${email}%0D%0AMessage: ${message}`;
+
+    const mailtoLink = `mailto:info@seprex.com?subject=${encodeURIComponent(
+      subject
+    )}&body=${encodeURIComponent(body)}`;
+
+    window.location.href = mailtoLink;
+  };
+
   return (
     <>
       <div className="footerMain">
@@ -19,8 +37,48 @@ export default function AppFooter() {
             Seprex is one-stop Enginering Solution Company setup at Pune India
             by passionate team of engineers.
           </p>
+
+          <h6>Address:</h6>
+          <p>
+            Office No.S-9,2nd Floor, Ashwarayam One, Kalbhor Pimpri-Chinchwad,
+            Pune-411019
+          </p>
+
+          <h6>Email:</h6>
+          <p>info@seprex.com</p>
         </div>
-        <div className="footerRight"></div>
+        <div className="footerRight">
+          <h3>Get in Touch</h3>
+          <InputGroup className="mb-3">
+            <Form.Control
+              id="nameInput"
+              aria-describedby="basic-addon3"
+              placeholder="Enter your name"
+            />
+          </InputGroup>
+          <InputGroup className="mb-3">
+            <Form.Control
+              id="emailInput"
+              aria-label="Amount (to the nearest dollar)"
+              placeholder="Enter your email"
+            />
+          </InputGroup>
+          <InputGroup>
+            <Form.Control
+              id="messageInput"
+              as="textarea"
+              aria-label="With textarea"
+              placeholder="Message..."
+            />
+          </InputGroup>
+          <Button
+            variant="info"
+            className="submitButton"
+            onClick={handleFormSubmit}
+          >
+            Submit
+          </Button>
+        </div>
       </div>
     </>
   );

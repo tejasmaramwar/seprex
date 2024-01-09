f52cdb0bff495707c59d8a777377c5af26fed9c9
jadhavhpranav1121 <jadhavhpranav1121@gmail.com>
2024-01-10 00:05:09 +0530

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
-import NavbarComp from "./components/NavbarComp";
 import AppFooter from "./components/Footer";
+import NavbarComp from "./components/NavbarComp";
 
 function App() {
   return (
     <>
+      <nav class="social">
+        <ul>
+          <li class="gmail"><i class="fa fa-envelope"  aria-hidden="true"></i>&nbsp;<a href="mailto:info@seprex.com">info@seprex.com</a></li>
+          <li class="phoneno"><i class="fa fa-phone" aria-hidden="true"></i> &nbsp;  <a href="tel:+919359429903">9359429903</a></li>
+        </ul>
+      </nav>
       <div className="App">
         <NavbarComp />
         <footer>
